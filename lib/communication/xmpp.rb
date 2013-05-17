90304e45111afcf731c29f7f71f5c2ca48ec49aa
Cooler_ <c00f3r@gmail.com>
2013-05-17 02:47:47 -0300

    Update xmpp.rb
    adaptação para hash do parse do openvas

--- lib/communication/xmpp.rb
+++ lib/communication/xmpp.rb
@@ -56,16 +56,16 @@ module Communication
 
         s.vulnerabilities do |vs|
           vs.vulnerability do |v|
-            v.hash issue[:_hash]
+          v.hash issue[:hash]
             v.title Base64.encode64(issue[:name].to_s)
-            v.description Base64.encode64("#{issue[:url]} \n\n #{issue[:description]}")
+            v.description Base64.encode64("#{issue[:host]} \n\n #{issue[:description]}")
             v.optional do |vo|
-              vo.affected_component Base64.encode64(issue[:affected_component])
-              vo.control Base64.encode64("#{issue[:remedy_guidance]} \n #{issue[:remedy_code]}")
-              vo.reference Base64.encode64(issue[:reference].to_s)
-              vo.reproduction Base64.encode64("#{issue[:cwe]} - #{issue[:cwe_url]}")
-              vo.exploitability issue[:severity].to_s.downcase
-              vo.template_id issue[:template_id].to_s.downcase
+           #   vo.affected_component Base64.encode64(issue[:affected_component])
+           #   vo.control Base64.encode64("#{issue[:remedy_guidance]} \n #{issue[:remedy_code]}")
+              vo.reference Base64.encode64(issue[:original_threat].to_s)
+              vo.reproduction Base64.encode64("#{issue[:cve]} ")
+              vo.exploitability issue[:risk_factor].to_s.downcase
+      #        vo.template_id issue[:template_id].to_s.downcase
             end
           end
         end
