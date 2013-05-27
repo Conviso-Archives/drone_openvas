1c24e15325ee39cfc9d9abb89dbf1d8c286301e5
Cooler_ <c00f3r@gmail.com>
2013-05-27 14:35:56 -0300

    Update openvas.rb
    i improve XML insertions

--- lib/parse/writer/openvas.rb
+++ lib/parse/writer/openvas.rb
@@ -1,3 +1,5 @@
+require 'rexml/document'
+include REXML
 
 module Parse
   module WRITER
@@ -5,14 +7,14 @@ module Parse
     @@file=0
 
      def initialize(x)
-     if File.exist?(x)
-      @@file=x
-      fd=File.new(x)
-      doc = Document.new fd
-      @@rows=doc.root
-     else
-      @ret= "Error in Load File!"
-     end 
+      if File.exist?(x)
+       @@file=x
+       fd=File.new(x)
+       doc = Document.new fd
+       @@rows=doc.root
+      else
+       @ret= "Error in Load File!"
+      end 
     end
 
    def write_xml(hash)
@@ -30,26 +32,30 @@ module Parse
     header.elements["duration"].text = hash[:scan_end]
    #vulnerabilidades
     start = Element.new("vulnerabilities")
-    hash[:results].each do |x|
+    hash[:issues].each do |x|
      vuln = doc.root.elements[1]
      vuln = Element.new("vulnerability")
+#     new(description="")
+      description=x[:description]
+ #    puts description
+ #    puts hash.inspect
 # para pegar valores do description e dividir em solution e reference e description
      text_description=[]
-     text_description=str.split(/\n\n/,5)
+     text_description=description.split(/\n\n/,5)
 #fazendo o xml
-     vuln.add_attribute("id", x["hash"])
+     vuln.add_attribute("id", x[:hash])
      vuln.add_element("title")
-     vuln.elements["title"].text=x["name"]
+     vuln.elements["title"].text=x[:name]
      vuln.add_element("description")
      vuln.elements["description"].text=text_description[1]
      vuln.add_element("category")
-     vuln.elements["category"].text=x["family"]
+     vuln.elements["category"].text=x[:family]
      vuln.add_element("impact")
-     vuln.elements["impact"].text=x["original_threat"]
+     vuln.elements["impact"].text=x[:original_threat]
      vuln.add_element("exploitability")
-     vuln.elements["exploitability"].text=x["original_threat"]
+     vuln.elements["exploitability"].text=x[:risk_factor]
      vuln.add_element("reproduction")
-     vuln.elements["reproduction"].text=x["cve"]
+     vuln.elements["reproduction"].text=x[:cve]
      vuln.add_element("control")
      vuln.elements["control"].text=text_description[2]
      vuln.add_element("reference")
