22f63110d8f434b4019a67e3a7c38ebd76683696
Cooler_ <c00f3r@gmail.com>
2013-06-20 22:49:00 -0300

    Update conviso.rb
    update para parsear o "description" para criar campo solution e references, também fix no nome para concatenar "Host".

--- lib/parse/writer/conviso.rb
+++ lib/parse/writer/conviso.rb
@@ -21,14 +21,45 @@ module Parse
           s.vulnerabilities do |vs|
             vs.vulnerability('id' => issue['_hash'] ) do |v|
               v.hash issue['_hash']
+              issue[:name] << " - Host: "+ issue[:host].to_s
               v.title Base64.encode64(issue[:name].to_s)
-              v.description Base64.encode64(issue[:description].to_s)
-             
+              issue[:description] << "Host: "+ issue[:host].to_s + "\n"
+              tmp=""
+              tmp=issue[:description].to_s
+              test=tmp.split("\n")
+              solution=""
+              references=""
+              description=""
+              counter=0
+               
+              test.each { |tmp|
+            	 if tmp.match('Solution:')
+  		          counter=1
+ 		           end
+ 
+          		 if tmp.match('References:')
+  		          counter=2
+ 		           end
+
+ 		           if counter == 0
+  		          description << tmp + "\n"
+		           end
+
+ 		           if counter == 1 
+                solution << tmp + "\n"
+ 		           end
+
+ 		           if counter == 2
+                references << tmp + "\n"
+               end
+              }
+              v.description Base64.encode64(description.to_s)
+       
               v.optional do |vo|
-                vo.impact issue[:risk_factor].to_s.downcase
-                vo.affected_component Base64.encode64(issue[:name].to_s)
-                vo.control Base64.encode64(issue[:description].to_s)
-                vo.reference Base64.encode64(issue[:description].to_s)
+                vo.impact issue[:risk_factor].to_s.downcase                
+                vo.affected_component Base64.encode64(issue[:name].to_s  )
+                vo.control Base64.encode64(solution.to_s)
+                vo.reference Base64.encode64(references.to_s)
                 vo.reproduction Base64.encode64(issue[:cve].to_s)
                 vo.exploitability issue[:risk_factor].to_s.downcase
                 vo.template_id issue[:template_id].to_s.downcase
@@ -42,5 +73,4 @@ module Parse
     end
   end
 end
-
 
